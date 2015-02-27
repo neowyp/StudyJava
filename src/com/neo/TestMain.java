ec46f39c6ec4333db32b888b5937859c22124f70
neowyp <wypsmall@126.com>
2015-02-27 13:50:04 +0800

    first commit

---
+++ src/com/neo/TestMain.java
@@ -0,0 +1,27 @@
+/**
+ * <p>Copyright: Copyright(c) 2015</p>
+ * <p>Company: gome.com.cn</p>
+ * <p>2015年2月27日下午1:49:25</p>
+ * @author wangyunpeng
+ * @version 1.0
+ */
+package com.neo;
+
+/** 
+ * desc:
+ * <p>创建人：wangyunpeng 创建日期：2015年2月27日下午1:49:25</p>
+ * @version V1.0  
+ */
+public class TestMain {
+
+	/**
+	 * desc:
+	 * <p>创建人：wangyunpeng , 2015年2月27日下午1:49:25</p>
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
